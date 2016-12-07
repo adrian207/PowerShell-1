eddfa2c057a024ca96ff1d11b442ad49806b6d4b
Todd Bush <todd64@users.noreply.github.com>
2016-12-07 11:37:32 -0600

    Add function Remove-SoftwareDistribution

--- Invoke-DiskCleanup.ps1
+++ Invoke-DiskCleanup.ps1
@@ -42,9 +42,8 @@ function Invoke-DiskCleanup {
 			
 			ForEach ($Computer in $ComputerName) {
 				$name = $Computer.ToUpper()
-				
 				ForEach ($key in $keys) {	
-					$hklm = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\{0}" -f $key
+					$hklm = Join-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches" -ChildPath $key
 					Write-Verbose -Message ( "{0}: Writing registry key {1}" -f $name,$hklm )
 					
 					Invoke-Command -ComputerName $name -ArgumentList $hklm -ScriptBlock {
@@ -75,7 +74,41 @@ function Invoke-DiskCleanup {
 			Write-Warning -Message ( "{0}: Something bad happened" -f $name )
 			Write-Warning -Message $Error[0].Exception.Message
 		}
-		
 	}
+}
+
+function Remove-SoftwareDistribution {
+	<#
+	.SYNOPSIS
+		This function removes the SoftwareDistribution folder and all subfolders from the Windows directory on a local or remote machine.
+		
+	.DESCRIPTION
+		This function removes the SoftwareDistribution folder and all subfolders from the Windows directory on a local or remote machine.
+		
+	.PARAMETER ComputerName
 	
+	.EXAMPLE
+		Remove-SoftwareDistribution
+	#>
+	
+	[CmdletBinding()]
+	
+	param(
+		[parameter(ValueFromPipeline=$True)]
+		[string[]]$ComputerName = $Env:ComputerName
+	)
+	
+	process {
+		ForEach ($Computer in $ComputerName) {
+			$Name = $Computer.ToUpper()
+			Invoke-Command -Computer $Name -ScriptBlock {
+				$SoftwareDistribution = Join-Path -Path $Env:WinDir -ChildPath SoftwareDistribution
+				$Splatting = @{
+					Path = $SoftwareDistribution
+					Recurse = $True
+				}
+				Remove-Item @Splatting
+			}
+		}
+	}
 }
