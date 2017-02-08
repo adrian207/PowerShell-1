3d908b9638c23a248ddca105100641a9366a93e3
Todd Bush <todd64@users.noreply.github.com>
2017-02-07 22:06:56 -0600

    Update Invoke-LockComputer.ps1

--- Invoke-LockComputer.ps1
+++ Invoke-LockComputer.ps1
@@ -1,53 +1,51 @@
 function Invoke-LockComputer
 {
-	<#
-	.SYNOPSIS
-		This function locks a local or remote machine.
-		
-	.DESCRIPTION
-		This function locks a local or remote machine.
-		
-	.PARAMETER ComputerName
-	
-	.EXAMPLE
-		Invoke-LockComputer
-		
-	.EXAMPLE
-		Invoke-LockComputer -ComputerName Computer1
-		
-	.EXAMPLE
-		Invoke-LockComputer -ComputerName Computer1,Computer2,Computer3
-		
-	.EXAMPLE
-		Get-Content C:\computers.txt | Invoke-LockComputer
-	#>
+<#
+.SYNOPSIS
+	This function locks a local or remote machine.
+
+.DESCRIPTION
+	This function locks a local or remote machine.
+
+.PARAMETER ComputerName
+
+.EXAMPLE
+	Invoke-LockComputer
+
+.EXAMPLE
+	Invoke-LockComputer -ComputerName COMPUTER1
+
+.EXAMPLE
+	Invoke-LockComputer -ComputerName COMPUTER1,COMPUTER2,COMPUTER3
+
+.EXAMPLE
+	Get-Content C:\computers.txt | Invoke-LockComputer
+#>
 
 	[CmdletBinding()]
 	
-	param(
+	Param(
 		[parameter(ValueFromPipeline=$True)]
 		[string[]]$ComputerName = $Env:ComputerName
 	)
 	
-	process {
+	Process {
 		Try {
 			ForEach ($Computer in $ComputerName) {
-				$name = $Computer.ToUpper()
-				Write-Verbose -Message ( "{0}: Locking the screen" -f $name )
-				$splatting = @{
+				$Name = $Computer.ToUpper()
+				Write-Verbose -Message ( "PROCCESS - {0} - Locking the screen" -f $Name )
+				$Splatting = @{
 					Class = "Win32_Process"
 					Name = "Create"
 					ArgumentList = "C:\Windows\System32\rundll32.exe user32.dll,LockWorkStation"
-					ComputerName = $name
+					ComputerName = $Name
 				}
-				Invoke-WmiMethod @splatting
+				Invoke-WmiMethod @Splatting
 			}
 		}
 		Catch {
-			Write-Warning -Message ( "{0}: Something bad happened" -f $name )
+			Write-Warning -Message ( "PROCESS - {0} - Something bad happened" -f $Name )
 			Write-Warning -Message $Error[0].Exception.Message
-		}
-		
+		}	
 	}
-
 }
