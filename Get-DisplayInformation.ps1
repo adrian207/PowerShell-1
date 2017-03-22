b53e62ffc865f00f3d2f3d04ab2a4dd01f11190c
Todd Bush <bushtodd@gmail.com>
2017-03-22 11:50:41 -0500

    Update Get-DisplayInformation.ps1

--- Get-DisplayInformation.ps1
+++ Get-DisplayInformation.ps1
@@ -28,68 +28,23 @@ function Get-DisplayInformation {
                 $Name = $Computer.ToUpper()
                 Write-Verbose -Message ( "PROCESS - {0} - Getting display information" -f $Name )
                 
-                $Splatting = @{
-                    #CimSession = New-CimSession -ComputerName $Name
-                }
-                # Get video card information
-                $VideoCards = Get-CimInstance -ClassName win32_VideoController
-                ForEach ($Card in $VideoCards) {
-                    $Info = [ordered]@{
-                        Name = $Card.Caption
-                        Driver = $Card.DriverVersion
-                        HorizontalResolution = $Card.CurrentHorizontalResolution
-                        VerticalResolution = $Card.CurrentVerticalResolution
-                    }
-                    New-Object -TypeName PSObject -Property $Info
-                }
+				$CimSession = New-CimSession -ComputerName $Name
 
                 # Get monitor information
-                $Monitors = Get-CimInstance -ClassName WmiMonitorId -Namespace "root\wmi"
+				$Splatting = @{
+					ClassName = "WmiMonitorId"
+					CimSession = $CimSession
+					Namespace = "root\wmi"
+				}
+                $Monitors = Get-CimInstance @Splatting
+
                 ForEach ($Monitor in $Monitors) {
-                    New-Object -TypeName PSObject -Property @{
-                        Manufacturer = ($Monitor.ManufacturerName -notmatch '^0$' | ForEach {[char]$_}) -join ""
+                    [pscustomobject]@{
                         Name = ($Monitor.UserFriendlyName -notmatch '^0$' | ForEach {[char]$_}) -join ""
+                        Manufacturer = ($Monitor.ManufacturerName -notmatch '^0$' | ForEach {[char]$_}) -join ""
                         Serial = ($Monitor.SerialNumberID -notmatch '^0$' | ForEach {[char]$_}) -join ""
                     }
                 }
-            }
-            Catch {
-                Write-Warning -Message ( "PROCESS - {0} - Something bad happened" -f $Name )
-				Write-Warning -Message $Error[0].Exception.Message
-            }
-        }
-    }
-}
-
-function Get-ScreenShot {
-<#
-.SYNOPSIS
-	This function gets a list of names for all computers in Active Directory.
-
-.DESCRIPTION
-	This function gets a list of names for all computers in Active Directory.
-
-.EXAMPLE
-	Get-ScreenShot -ComputerName COMPUTER01
-
-.EXAMPLE
-    Get-ScreenShot -ComputerName COMPUTER01,COMPUTER02,COMPUTER03
-#>
-
-    [CmdletBinding()]
-
-    Param(
-        [parameter(ValueFromPipeline=$True)]
-        [String[]]$ComputerName = $env:ComputerName
-    )
-
-    Process {
-        ForEach ($Computer in $ComputerName) {
-            Try {
-                $Name = $Computer.ToUpper()
-                Write-Verbose -Message ( "PROCESS - {0} - Getting display information" -f $Name )
-
-
             }
             Catch {
                 Write-Warning -Message ( "PROCESS - {0} - Something bad happened" -f $Name )
