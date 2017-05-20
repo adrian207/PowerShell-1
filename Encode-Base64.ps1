179f98ff076c68a42bb3aa72cfc23f4a06da5525
Todd Bush <todd64@users.noreply.github.com>
2017-05-19 19:54:57 -0500

    Update Encode-Base64.ps1

--- Encode-Base64.ps1
+++ Encode-Base64.ps1
@@ -1,4 +1,4 @@
-﻿function Encode-Base64 {
+function Encode-Base64 {
 <#
 .SYNOPSIS
 	This function takes a string and encodes it to base64.
@@ -7,24 +7,26 @@
 	This function takes a string and encodes it to base64.
 
 .EXAMPLE
-    Encode-Base64 "The quick brown fox jumps over the lazy dog."
+	Encode-Base64 "The quick brown fox jumps over the lazy dog."
+    
+	VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4=
 
 .EXAMPLE
 	Out-File -FilePath c:\something.txt | Encode-Base64
 #>
 
 	[CmdletBinding()]
-	
+
 	Param(
-        [Parameter(ValueFromPipeline = $True)]
+	[Parameter(ValueFromPipeline = $True)]
 		[String]$Object
-    )
+	)
 	
 	Process {
 		Try {
-            Write-Verbose -Message "[ConvertTo-Base64] Converting image to Base64 $Object"
-	        [System.convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($Object))
-        }
+            		Write-Verbose -Message "[ConvertTo-Base64] Converting image to Base64 $Object"
+	       		[System.convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($Object))
+        	}
 		Catch {
 			Write-Warning "PROCESS - Something bad happened"
 			Write-Warning -Message $Error[0].Exception.Message
@@ -41,9 +43,9 @@ function Decode-Base64 {
 	This function takes a base64 string and decodes it.
 
 .EXAMPLE
-    Decode-Base64 VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4=
+	Decode-Base64 VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4=
 
-    The quick brown fox jumps over the lazy dog.
+	The quick brown fox jumps over the lazy dog.
 
 .EXAMPLE
 	Out-File -FilePath c:\something.txt | Decode-Base64
@@ -52,15 +54,15 @@ function Decode-Base64 {
 	[CmdletBinding()]
 	
 	Param(
-        [Parameter(ValueFromPipeline = $True)]
+        	[Parameter(ValueFromPipeline = $True)]
 		[String]$Object
-    )
+    	)
 	
 	Process {
 		Try {
-            Write-Verbose -Message "[ConvertTo-Base64] Converting image to Base64 $Object"
-            [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Object))
-        }
+            		Write-Verbose -Message "[ConvertTo-Base64] Converting image to Base64 $Object"
+            		[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Object))
+        	}
 		Catch {
 			Write-Warning "PROCESS - Something bad happened"
 			Write-Warning -Message $Error[0].Exception.Message
