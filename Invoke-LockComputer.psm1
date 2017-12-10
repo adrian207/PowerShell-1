5041232acb684d90caa5955d756e3e481409b7d7
Todd Bush <bushtodd@gmail.com>
2017-12-10 13:12:21 -0600

    Rename Invoke-LockComputer.ps1 to Invoke-LockComputer.psm1

---
+++ Invoke-LockComputer.psm1
@@ -0,0 +1,51 @@
+function Invoke-LockComputer {
+<#
+.SYNOPSIS
+	This function locks a local or remote machine.
+
+.DESCRIPTION
+	This function locks a local or remote machine.
+
+.PARAMETER ComputerName
+
+.EXAMPLE
+	Invoke-LockComputer
+
+.EXAMPLE
+	Invoke-LockComputer -ComputerName COMPUTER1
+
+.EXAMPLE
+	Invoke-LockComputer -ComputerName COMPUTER1,COMPUTER2,COMPUTER3
+
+.EXAMPLE
+	Get-Content C:\computers.txt | Invoke-LockComputer
+#>
+
+	[CmdletBinding()]
+	
+	param(
+		[parameter(ValueFromPipeline=$True)]
+		[string[]]$ComputerName = $Env:ComputerName
+	)
+	
+	process {
+		foreach ($Computer in $ComputerName) {
+			try {
+				$Name = $Computer.ToUpper()
+				Write-Verbose -Message ( "PROCCESS - {0} - Locking the screen" -f $Name )
+				$CimClass = Get-CimClass -ClassName Win32_Process
+				$Splatting = @{
+					CimClass = $CimClass
+					MethodName = "Create"
+					Arguments = "C:\Windows\System32\rundll32.exe user32.dll,LockWorkStation"
+					ComputerName = $Name
+				}
+				Invoke-CimMethod @Splatting
+			}
+			catch {
+				Write-Warning -Message ( "PROCESS - {0} - Something bad happened" -f $Name )
+				Write-Warning -Message $Error[0].Exception.Message
+			}
+		}
+	}
+}
