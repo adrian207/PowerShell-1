7c444512578763d4684df63106ed1805fb95accc
Todd Bush <bushtodd@gmail.com>
2018-02-04 08:58:09 -0600

    Move Invoke-LockComputer.psm1 to its own directory

--- Invoke-LockComputer/Invoke-LockComputer.psm1
+++ Invoke-LockComputer/Invoke-LockComputer.psm1
@@ -11,14 +11,22 @@ function Invoke-LockComputer {
 .EXAMPLE
     Invoke-LockComputer
 
+    Locks the local machine.
+
 .EXAMPLE
     Invoke-LockComputer -ComputerName COMPUTER01
     
+    Locks the remote machine COMPUTER01.
+
 .EXAMPLE
     Invoke-LockComputer -ComputerName COMPUTER01,COMPUTER02,COMPUTER03
 
+    Locks the remote machines COMPUTER01, COMPUTER02, and COMPUTER03.
+
 .EXAMPLE
     Get-Content C:\computers.txt | Invoke-LockComputer
+
+    Locks the remote machines listed in the text file.
 #>
 
     [CmdletBinding()]
@@ -40,20 +48,19 @@ function Invoke-LockComputer {
                     CommandLine = "C:\Windows\System32\rundll32.exe user32.dll,LockWorkStation"
                 }
             }
-            if ( $Name -ne $env:ComputerName) {
-				$Splatting.Add("ComputerName",$Name)
+            if ($Name -ne $env:ComputerName) {
+                $Splatting.ComputerName = $Name
 
-				if ( Test-Connection -ComputerName $Name -Count 1 ) {
-					Write-Verbose -Message ( "PROCCESS - {0} - Locking the screen" -f $Name )
-				}
-				else {
-					Write-Warning -Message ( "PROCESS - {0} - Failed to connect to host" -f $Name )
-					continue
-				}
+                if ( Test-Connection -ComputerName $Name -Count 1 ) {
+                    Write-Verbose -Message ( "PROCESS - {0} - Locking the screen" -f $Name )
+                }
+                else {
+                    Write-Warning -Message ( "PROCESS - {0} - Failed to connect to host" -f $Name )
+                    continue
+                }
+            }
 
-			}
-			
-			Invoke-CimMethod @Splatting
+            Invoke-CimMethod @Splatting
         }
     }
 }
