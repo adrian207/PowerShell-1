e49836c6d523855bddbe4b384fbb4d525e2e611e
Todd Bush <todd64@users.noreply.github.com>
2018-02-08 11:08:18 -0600

    Update Invoke-LockComputer.psm1

--- Invoke-LockComputer/Invoke-LockComputer.psm1
+++ Invoke-LockComputer/Invoke-LockComputer.psm1
@@ -33,31 +33,30 @@ function Invoke-LockComputer {
     
     param(
         [Parameter(
-            ValueFromPipeline=$true)]
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
         [string[]]$ComputerName = $env:ComputerName
     )
     
+    begin {
+        $Splatting = @{
+            ClassName = 'Win32_Process'
+            MethodName = 'Create'
+            Arguments = @{
+                CommandLine = 'C:\Windows\System32\rundll32.exe user32.dll,LockWorkStation'
+            }
+        }
+    }
+
     process {
         foreach ($Computer in $ComputerName) {
             $Name = $Computer.ToUpper()
-
-            $Splatting = @{
-                ClassName = "Win32_Process"
-                MethodName = "Create"
-                Arguments = @{
-                    CommandLine = "C:\Windows\System32\rundll32.exe user32.dll,LockWorkStation"
-                }
-            }
             if ($Name -ne $env:ComputerName) {
                 $Splatting.ComputerName = $Name
-
-                if ( Test-Connection -ComputerName $Name -Count 1 ) {
-                    Write-Verbose -Message ( "PROCESS - {0} - Locking the screen" -f $Name )
-                }
-                else {
-                    Write-Warning -Message ( "PROCESS - {0} - Failed to connect to host" -f $Name )
-                    continue
-                }
+            }
+            else {
+                $Splatting.ComputerName = $null
             }
 
             Invoke-CimMethod @Splatting
