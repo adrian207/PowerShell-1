59c084564a3b946240845d6ab7543a0315bc45d6
Todd Bush <bushtodd@gmail.com>
2018-02-10 19:52:15 -0600

    Update New-Password.psm1

--- New-Password/New-Password.psm1
+++ New-Password/New-Password.psm1
@@ -1,11 +1,11 @@
 function New-Password {
 <#
 .SYNOPSIS
-	This function generates a random password.
+    This function generates a random password.
 
 .DESCRIPTION
     This function generates a random password.
-	
+    
 .PARAMETER Length
     The number of characters in the generated password. The length must be between 1 and 128 characters.
     Default is 12.
@@ -15,8 +15,8 @@ function New-Password {
     Default is 5.
 
 .PARAMETER Count
-	Specifies how many passwords you want to generate.
-	Default is 1.
+    Specifies how many passwords you want to generate.
+    Default is 1.
 
 .EXAMPLE
     New-Password
@@ -24,28 +24,35 @@ function New-Password {
 
     [CmdletBinding()]
 
-	param (
-		[parameter()]
-		[Int32]$Length = 12,
-		
-		[parameter()]
-		[Int32]$NumberOfNonAlphanumericCharacters = 5,
-		
-		[parameter()]
-		[Int32]$Count = 1
-	)
-	
-	begin {
-		Add-Type -AssemblyName System.Web
-	}
-	
-	process	{
-        $ScriptBlock = {
-            [System.Web.Security.Membership]::GeneratePassword(
-				$Length, $NumberOfNonAlphanumericCharacters)
-        }
+    param (
+        [parameter(
+            Mandatory=$false,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $Length = 12,
         
-		$Password = (1..$Count) | ForEach-Object -Process $ScriptBlock
-        Set-Clipboard -Value $Password
-	}
+        [parameter(
+            Mandatory=$false,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $NumberOfNonAlphanumericCharacters = 5,
+        
+        [parameter(
+            Mandatory=$false,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $Count = 1
+	)
+    
+    process    {
+        Add-Type -AssemblyName System.Web
+        
+        for ($i = 1; $i -le $Count; $i++) {
+            [System.Web.Security.Membership]::GeneratePassword(
+                $Length, $NumberOfNonAlphanumericCharacters)
+        }
+    }
 }
