4c8067caf3de4cc128de186567425eefb12e124e
Todd Bush <bushtodd@gmail.com>
2018-02-24 20:05:54 -0600

    Get-DisplayInformation.psm1: Add `Get-MonitorConnection` function

--- Get-DisplayInformation/Get-DisplayInformation.psm1
+++ Get-DisplayInformation/Get-DisplayInformation.psm1
@@ -1,17 +1,16 @@
-﻿#requires -version 5
-function Get-DisplayInformation {
+﻿function Get-DisplayInformation {
 <#
 .SYNOPSIS
-	This function gets the display information from a local or remote machine.
+    This function gets the display information from a local or remote machine.
 
 .DESCRIPTION
-	This function gets the display information from a local or remote machine.
+    This function gets the display information from a local or remote machine.
 
 .EXAMPLE
-	Get-DisplayInformation
+    Get-DisplayInformation
 
 .EXAMPLE
-	Get-DisplayInformation -ComputerName COMPUTER01
+    Get-DisplayInformation -ComputerName COMPUTER01
 
 .EXAMPLE
     Get-DisplayInformation -ComputerName COMPUTER01,COMPUTER02,COMPUTER03
@@ -27,22 +26,19 @@ function Get-DisplayInformation {
             Mandatory=$false,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
+        [Alias('IPAddress','__Server','CN')]
         [string[]]
         $ComputerName = $env:ComputerName
     )
 
-    begin {
+    process {
         $Splatting = @{
             ClassName = 'WmiMonitorId'
             Namespace = 'root\wmi'
         }
-    }
 
-    process {
         foreach ($Computer in $ComputerName) {
             $Name = $Computer.ToUpper()
-            Write-Verbose -Message ( "PROCESS - {0} - Getting display information" -f $Name )
-
             if ($Name -ne $env:ComputerName) {
                 $Splatting.ComputerName = $Name
             }
@@ -50,13 +46,68 @@ function Get-DisplayInformation {
                 $Splatting.ComputerName = $null
             }
 
+            Write-Verbose -Message ( "{0} - Getting display information" -f $Name )
             foreach ( $_ in (Get-CimInstance @Splatting) ) {
-                [PSCustomObject]@{
-                    ComputerName = $Name
-                    Name = ($_.UserFriendlyName -notmatch '^0$' | ForEach {[char]$_}) -join ""
-                    Manufacturer = ($_.ManufacturerName -notmatch '^0$' | ForEach {[char]$_}) -join ""
-                    Serial = ($_.SerialNumberID -notmatch '^0$' | ForEach {[char]$_}) -join ""
+
+                switch ($_.ManufacturerName) {
+                    condition { $ManufacturerName = '' }
+                    Default { $ManufacturerName = 'Unknown' }
                 }
+
+                [PSCustomObject]@{
+                    PSTypeName        = 'DisplayInformation'
+                    ComputerName      = $Name
+                    InstanceName      = $_.InstanceName
+                    UserFriendlyName  = ($_.UserFriendlyName -notmatch '^0$' | ForEach {[char]$_}) -join ''
+                    ManufacturerName  = ($_.ManufacturerName -notmatch '^0$' | ForEach {[char]$_}) -join ''
+                    ProductCodeID     = ($_.ProductCodeID -notmatch '^0$' | ForEach {[char]$_}) -join ''
+                    SerialNumberID    = ($_.SerialNumberID -notmatch '^0$' | ForEach {[char]$_}) -join ''
+                    WeekOfManufacture = $_.WeekOfManufacture
+                    YearOfManufacture = $_.YearOfManufacture
+                    OutputType        = Get-MonitorConnection -InstanceName $_.InstanceName
+                }
+            }
+        }
+    }
+}
+
+function Get-MonitorConnection {
+    param(
+        [string]
+        $ComputerName,
+
+        [string]
+        $InstanceName
+    )
+
+    process {
+        $Splatting = @{
+            ClassName = 'WmiMonitorConnectionParams'
+            Namespace = 'root\wmi'
+        }
+        if ($Computer -ne $env:ComputerName) {
+            $Splatting.ComputerName = $Computer
+        }
+
+        foreach ($_ in Get-CimInstance @Splatting) {
+            if ($InstanceName -eq $_.InstanceName) {
+                # https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/content/d3dkmdt/ne-d3dkmdt-_d3dkmdt_video_output_technology
+                switch ($_.VideoOutputTechnology) {
+                    -2 { $OutputType = 'Uninitialized' }
+                    0 { $OutputType = 'VGA' }
+                    1 { $OutputType = 'S-Video' }
+                    2 { $OutputType = 'Composite Video' }
+                    3 { $OutputType = 'Component Video' }
+                    4 { $OutputType = 'DVI' }
+                    5 { $OutputType = 'HDMI' }
+                    6 { $OutputType = 'LVDS' }
+                    # 8 { $OutputType = 'D_JPN' }
+                    9 { $OutputType = 'SDI' }
+                    10 { $OutputType = 'DisplayPort External' }
+                    11 { $OutputType = 'DisplayPort' }
+                    Default { $OutputType = 'Other' }
+                }
+                $OutputType
             }
         }
     }
@@ -71,16 +122,16 @@ function Get-VideoCard {
     This function gets a list of video cards from a local or remote machine.
 
 .EXAMPLE
-    Get-DisplayInformation
+    Get-VideoCard
 
 .EXAMPLE
-    Get-DisplayInformation -ComputerName COMPUTER01
+    Get-VideoCard -ComputerName COMPUTER01
 
 .EXAMPLE
-    Get-DisplayInformation -ComputerName COMPUTER01,COMPUTER02,COMPUTER03
+    Get-VideoCard -ComputerName COMPUTER01,COMPUTER02,COMPUTER03
 
 .EXAMPLE
-    Out-Content -Path c:\something.txt | Get-DisplayInformation
+    Out-Content -Path c:\something.txt |Get-VideoCard
 #>
 
     [CmdletBinding()]
@@ -91,28 +142,20 @@ function Get-VideoCard {
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true,
             Position=0)]
+        [Alias('IPAddress','__Server','CN')]
         [string[]]
         $ComputerName = $env:ComputerName
     )
 
-    begin {
-        $Splatting = @{
-            ClassName = 'Win32_VideoController'
-        }
-    }
-
     process {
         foreach ($Computer in $ComputerName) {
+            $Splatting = @{
+                ClassName = 'CIM_VideoController'
+                ComputerName = $null
+            }
+
             $Name = $Computer.ToUpper()
-            Write-Verbose -Message ( "PROCESS - {0} - Getting video card information" -f $Name )
-
-            if ($Name -ne $env:ComputerName) {
-                $Splatting.ComputerName = $Name
-            }
-            else {
-                $Splatting.ComputerName = $null
-            }
-
+            Write-Verbose -Message ( "{0} - Getting video card information" -f $Name )
             foreach ( $_ in (Get-CimInstance @Splatting) ) {
                 switch ($_.Availability) {
                     1 { $Availability = 'Other' }
@@ -123,7 +166,7 @@ function Get-VideoCard {
                     7 { $Availability = 'Power Off' }
                     8 { $Availability = 'Offline' }
                     9 { $Availability = 'Off Duty' }
-                    10 { $Availability = 'Degraded ' }
+                    10 { $Availability = 'Degraded' }
                     11 { $Availability = 'Not Installed' }
                     12 { $Availability = 'Install Error' }
                     13 { $Availability = 'Power Save - Unknown' }
@@ -138,19 +181,35 @@ function Get-VideoCard {
                     Default { $Availability = 'Unknown' }
                 }
 
+                switch ($_.VideoMemoryType) {
+                    1 { $VideoMemoryType = 'Other' }
+                    3 { $VideoMemoryType = 'VRAM' }
+                    4 { $VideoMemoryType = 'DRAM' }
+                    5 { $VideoMemoryType = 'SRAM' }
+                    6 { $VideoMemoryType = 'WRAM' }
+                    7 { $VideoMemoryType = 'EDO RAM' }
+                    8 { $VideoMemoryType = 'Burst Synchronous DRAM' }
+                    9 { $VideoMemoryType = 'Pipelined Burst SRAM' }
+                    10 { $VideoMemoryType = 'CDRAM' }
+                    11 { $VideoMemoryType = '3DRAM' }
+                    12 { $VideoMemoryType = 'SDRAM' }
+                    13 { $VideoMemoryType = 'SGRAM' }
+                    Default { $VideoMemoryType = 'Unknown' }
+                }
+
                 [PSCustomObject]@{
                     PSTypeName              = 'PSVideoController'
                     ComputerName            = $Name
                     Caption                 = $_.Caption
                     Status                  = $_.Status
                     Availability            = $Availability
-                    Driver                  = $_.DriverVersion
+                    DriverVersion           = $_.DriverVersion
                     DriverDate              = $_.DriverDate
                     HorizontalResolution    = $_.CurrentHorizontalResolution
                     VerticalResolution      = $_.CurrentVerticalResolution
                     CurrentBitsPerPixel     = $_.CurrentBitsPerPixel
-                    'RAM Type'              = $_.VideoMemoryType
-                    'Amount of RAM'         = $_.AdapterRAM
+                    VideoMemoryType         = $VideoMemoryType
+                    AdapterRAM              = $_.AdapterRAM
                     InstalledDisplayDrivers = ($_.InstalledDisplayDrivers).Split(',')
                 }
             }
