3e1d15d338276cdf7bc7122700d97d7f6e7c2be3
Todd Bush <bushtodd@gmail.com>
2018-03-03 17:16:44 -0600

    Add Convert-BytesToSize.psm1

---
+++ Convert-BytesToSize/Convert-BytesToSize.psm1
@@ -0,0 +1,66 @@
+function Convert-BytesToSize {
+<#
+.SYNOPSIS
+    Converts any integer size given to a user friendly size.
+
+.DESCRIPTION
+    Converts any integer size given to a user friendly size.
+
+.PARAMETER Size
+
+.EXAMPLE
+    PS> Convert-BytesToSize -Size 134217728
+
+    Converts size to show 128MB
+#>
+
+    [CmdletBinding()]
+
+    param(
+        [parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position = 0)]
+        [int64]
+        $Size,
+
+        [parameter(
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true,
+            Position = 1)]
+        [int]
+        $Precision = 2
+    )
+
+    #Decide what is the type of size
+    switch ($Size) {
+        {$Size -gt 1PB} {
+            $NewSize = "$([math]::Round(($Size / 1PB),$Precision))PB"
+            break
+        }
+        {$Size -gt 1TB} {
+            $NewSize = "$([math]::Round(($Size / 1TB),$Precision))TB"
+            break
+        }
+        {$Size -gt 1GB} {
+            $NewSize = "$([math]::Round(($Size / 1GB),$Precision))GB"
+            break
+        }
+        {$Size -gt 1MB} {
+            $NewSize = "$([math]::Round(($Size / 1MB),$Precision))MB"
+            break
+        }
+        {$Size -gt 1KB} {
+            $NewSize = "$([math]::Round(($Size / 1KB),$Precision))KB"
+            break
+        }
+        Default {
+            $NewSize = "$([math]::Round($Size,$Precision))Bytes"
+        }
+    }
+
+    [PSCustomObject]@{
+        PSTypeName = 'ConvertBytesToSize'
+        NewSize    = $NewSize
+    }
+}
